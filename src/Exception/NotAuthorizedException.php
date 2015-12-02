adfbf1d17a23cf450e3e5ac997fdf4bcceae4fd5
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2015-12-02 17:49:35 +0100

    add exception NotAuthorizedException

---
+++ src/Exception/NotAuthorizedException.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Gobline Framework
+ *
+ * (c) Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gobline\Acl\Exception;
+
+/**
+ * @author Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
+ */
+class NotAuthorizedException extends \RuntimeException 
+{
+}
